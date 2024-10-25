bd4b435599d7149006706535ed84a8fa7af7be7e
Alex Duzi <alex.henrique.duzi>
2024-10-25 17:23:16 -0300

    CategoryController update controller mapping bugfix

--- shopping-cart/src/main/java/com/alexduzi/shoppingcart/controller/CategoryController.java
+++ shopping-cart/src/main/java/com/alexduzi/shoppingcart/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -86,7 +87,7 @@ public class CategoryController {
 		}
 	}
 	
-	@DeleteMapping("/category/{id}/update")
+	@PutMapping("/category/{id}/update")
 	public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category) {
 		try {
 			Category updatedCategory = categoryService.updateCategory(category, id);
